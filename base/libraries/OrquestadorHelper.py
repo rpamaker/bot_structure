fcee4d13fde67c8df72d3e36f2af8058e71f3689
Nicolas <nicolas@rpamaker.com>
2022-03-11 22:18:00 -0300

    bot structure fastapi

--- base/libraries/OrquestadorHelper.py
+++ base/libraries/OrquestadorHelper.py
@@ -2,6 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 import logging
 
+
 # ref: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
 def upload_log_s3(file_name, bucket, object_name=None):
     """Upload a file to an S3 bucket
@@ -15,7 +16,7 @@ def upload_log_s3(file_name, bucket, object_name=None):
         if object_name is None:
             object_name = file_name
         # Upload the file
-        s3_client = boto3.client('s3')    
+        s3_client = boto3.client('s3')
         response = s3_client.upload_file(file_name, bucket, object_name, ExtraArgs={'ContentType': 'text/html'})
     except ClientError as e:
         logging.error(e)
