f3a63e29e67e074c3a20da92d1319c9aec1b7412
Andres Baston <andres@rpamaker.com>
2023-02-15 11:46:53 -0300

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-export DOCKERHUB_REPO=danielaron/demo-rpamaker
-export BUILD_VERSION=1
-
-docker build -t $DOCKERHUB_REPO:$BUILD_VERSION .
-docker push $DOCKERHUB_REPO:$BUILD_VERSION
