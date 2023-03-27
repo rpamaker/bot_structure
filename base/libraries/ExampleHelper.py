170729f1657f1bbf1c6546e3bfeba5562074c6ea
Andres Baston <andres@rpamaker.com>
2023-03-27 15:58:21 -0300

    Update ExampleHelper.py

--- base/libraries/ExampleHelper.py
+++ base/libraries/ExampleHelper.py
@@ -1,7 +1,9 @@
 from time import sleep
-
+from decouple import config
 from robot.api import logger
 
+def get_environment(var):
+    return config(var)
 
 def example_python_keyword():
     logger.warn("HOLAMUNDO DESDE PYTHON")
